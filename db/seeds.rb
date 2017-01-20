657aba06291640b92f9558fdb9f4aee655c0825d
Laura Kibben <smartgirlpdx@gmail.com>
2017-01-20 10:36:50 -0800

    changed seeding code back after troubleshooting Heroku issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 I18n.reload!
 
  10.times do
-   teamname = "teamname"
+   teamname = Faker::Superhero.power
    team = Team.create!(
      name: teamname,
      andon: 0,
@@ -16,7 +16,7 @@ I18n.reload!
      accounting_code: Faker::Number.number(3),
      jira_project_key: Faker::Number.number(4),
      email: teamname + "@newrelic.com",
-     slack_channel: teamname + "slackchannel"
+     slack_channel: teamname + " " + Faker::StarWars.specie,
      )
     team.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
  end
