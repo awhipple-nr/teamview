57fc169eac519d35146f42c70fc96f945723a412
Laura Kibben <smartgirlpdx@gmail.com>
2017-01-20 17:03:24 -0800

    styling and navbar fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
      content_tag :div, capture(&block), class: css_class
   end
 
+  def nav_bar
+    content_tag(:ul, class: "nav navbar-nav") do
+      yield
+    end
+  end
+
+  def nav_link(text, path)
+    options = current_page?(path) ? { class: "active" } : {}
+      content_tag(:li, options) do
+        link_to text, path
+    end
+  end
+
 end
